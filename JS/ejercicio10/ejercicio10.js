7f860f2cf83d62058a6a010ab549c0b03c7efa03
Brayan <brayanfernandezjimenez18@gmail.com>
2025-02-28 07:17:42 -0500

    desarrollo ejercicio 10

---
+++ JS/ejercicio10/ejercicio10.js
@@ -0,0 +1,14 @@
+// La función incorporada setTimeout utiliza callbacks. Crea una alternativa basada en
+// promesas.
+// La función delay(ms) debería devolver una promesa. Esa promesa debería resolverse
+// después de ms milisegundos, para que podamos agregarle. then, así:
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Se ejecuto")
+    }, ms);
+  })
+}
+
+delay(3000).then(alert);
