17e9b45b7f1c5d3347d3517456c6df1c35f3a7bc
Brayan <brayanfernandezjimenez18@gmail.com>
2025-03-05 07:07:13 -0500

    desarrollo ejercicio 15

---
+++ JS/ejercicio15/ejercicio15.js
@@ -0,0 +1,10 @@
+// Realiza una llamada a un archivo local Muestra en formato json, luego muestre los datos
+// obtenidos en la consola.
+
+fetch("../JSON/ejercicio15.json").then(function (data) {
+  // console.log(data)
+  const respuesta = data.json().then(function (dato) {
+    console.log(dato)
+  })
+})
+
